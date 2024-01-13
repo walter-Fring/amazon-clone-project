3b854e81bbd95daad450b3b9161b09b023d93295
jordanlee <jordanlee@gmail.com>
2024-01-13 19:06:04 +0800

    create automated tests for formatCurrency func

---
+++ tests/testFormatPrice.js
@@ -0,0 +1,16 @@
+import formatPrice from '../scripts/utils/formatPrice.js';
+
+function testFormatPrice(price, testCase, caseName) {
+  console.log(caseName);
+
+  if (formatPrice(price) === testCase) {
+    console.log('passed');
+  } else {
+    console.log('failed');
+  }
+}
+
+testFormatPrice(2095, '20.95', 'converts cents into dollars');
+testFormatPrice(0, '0.00', 'works with 0');
+testFormatPrice(2000.5, '20.01', 'rounds up to the nearest cent');
+testFormatPrice(2000.4, '20.00', 'rounds down to the nearest cent');
