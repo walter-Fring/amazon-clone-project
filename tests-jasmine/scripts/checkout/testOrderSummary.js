7a60a83431c80a2e8f57c3e4d7edf03d007aa527
jordanlee <jordanlee@gmail.com>
2024-01-16 09:53:51 +0800

    create automated tests for order summary

--- tests-jasmine/scripts/checkout/testOrderSummary.js
+++ tests-jasmine/scripts/checkout/testOrderSummary.js
@@ -58,12 +58,12 @@ describe('test suite: renderOrderSummary', () => {
     ).toEqual(1);
 
     expect(
-      document.querySelector(`js-cart-item-container-${productId1}`)
+      document.querySelector(`.js-cart-item-container-${productId1}`)
     ).toEqual(null);
 
-    // expect(
-    //   document.querySelector(`js-cart-item-container-${productId2}`)
-    // ).not.toEqual(null);
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId2}`)
+    ).not.toEqual(null);
 
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
