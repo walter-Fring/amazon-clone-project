e6b0dfef0f6c88aa1a455c95548dc0a3bdd4d59d
jordanlee <jordanlee@gmail.com>
2024-04-10 09:22:47 +0800

    use inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -34,6 +34,27 @@ class Product {
   getPrice() {
     return `$${formatPrice(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
+};
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size Chart
+      </a>
+    `;
+  }
 };
 
 export const products = [
@@ -709,6 +730,10 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
+
   return new Product(productDetails);
 });
 
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -20,16 +20,20 @@ function renderProducts() {
         <div class="product-image-container">
           <img class="product-image" src="${product.image}" alt="product image">
         </div>
+
         <div class="product-name-container">
           <p class="product-name limit-text-to-2-lines">
             ${product.name}
           </p>
         </div>
+
         <div class="product-rating-container">
           <img class="product-rating-stars" src="${product.getStarsURL()}" alt="product rating stars">
           <div class="product-rating-count">${product.rating.count}</div>
         </div>
+
         <div class="product-price">${product.getPrice()}</div>
+        
         <div class="product-quantity-container">
           <select class="js-quantity-selector-${product.id} quantity-selector" name="quantity">
             <option selected value="1">1</option>
@@ -44,10 +48,16 @@ function renderProducts() {
             <option value="10">10</option>
           </select>
         </div>
+        
+        ${product.extraInfoHTML()}
+
+        <div class="product-spacer"></div>
+
         <div class="js-added-to-cart-message-${product.id} added-to-cart-message">
           <img src="images/icons/checkmark.png" alt="checkmark icon">
           <div>Added</div>
         </div>
+        
         <div class="add-to-cart-btn-container">
           <button class="js-add-to-cart-btn add-to-cart-btn primary-btn" data-product-id="${product.id}">
             Add to Cart
